764bdb0089d70f2d0d6ef3b7d468fd30dbf4acdb
Leandro <leandrom@lagash.com>
2019-10-11 20:39:45 -0300

    Add models

---
+++ src/model/Property.java
@@ -0,0 +1,79 @@
+package model;
+
+import java.util.List;
+import model.Address;
+
+public class Property {
+
+	private int type;
+	
+	private Address address;
+	
+	private List<String> services;
+	
+	private int capacity;
+	
+	private String userId;
+	
+	/**
+	 * @return the type
+	 */
+	public int getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(int type) {
+		this.type = type;
+	}
+	/**
+	 * @return the services
+	 */
+	public List<String> getServices() {
+		return services;
+	}
+	/**
+	 * @param services the services to set
+	 */
+	public void setServices(List<String> services) {
+		this.services = services;
+	}
+	/**
+	 * @return the capacity
+	 */
+	public int getCapacity() {
+		return capacity;
+	}
+	/**
+	 * @param capacity the capacity to set
+	 */
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+}
--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,53 @@
 package model;
 
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author leandrom
+ *
+ */
 public class User {
 
+	private String id = UUID.randomUUID().toString();
+			
+	private String email;
+	
+	private String fullName;
+	
+	private String phoneNumber;
+	
+	public User(final String fullName, final String email, final String phoneNumber) {
+		this.fullName = fullName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	
+	/**
+	 * @return the fullName
+	 */
+	public String getFullName() {
+		return fullName;
+	}
+	
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
 }
